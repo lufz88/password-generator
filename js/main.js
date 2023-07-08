d61d43385b54b424db192ddabd731a974bdeec37
lufz <lucasfrazzetta@gmail.com>
2023-07-08 19:43:40 -0300

    funciones para generar la contraseña y mostrarla

--- js/main.js
+++ js/main.js
@@ -79,3 +79,55 @@ const symbols = [
 	'{',
 	'}',
 ];
+
+const form = document.querySelector('#form');
+const lengthInput = document.querySelector('#length');
+const lengthSpan = document.querySelector('#show-length');
+const passwordSpan = document.querySelector('#password');
+const copyButton = document.querySelector('#copy-to-clipboard');
+
+lengthSpan.innerText = lengthInput.value;
+
+function generatePassword(target, length) {
+	const passwordLength = length;
+	let password = '';
+	const charactersIncluded = [];
+	target.upLetters.checked && charactersIncluded.push(upperCaseLetters);
+	target.lowLetters.checked && charactersIncluded.push(lowerCaseLetters);
+	target.numbers.checked && charactersIncluded.push(numbers);
+	target.symbols.checked && charactersIncluded.push(symbols);
+
+	if (charactersIncluded.length < 1) {
+		alert('Seleccioná algo loco');
+		return;
+	}
+
+	for (let i = 0; i < passwordLength; i++) {
+		const characterArray = charactersIncluded[randomNumber(0, charactersIncluded.length)];
+		const character = characterArray[randomNumber(0, characterArray.length)];
+		password += character;
+	}
+	passwordSpan.value = password;
+}
+
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min));
+}
+
+function copyToClipboard(target) {
+	const element = document.querySelector(target);
+	const value = element.value;
+
+	window.navigator.clipboard.writeText(value);
+}
+
+lengthInput.addEventListener('change', () => (lengthSpan.innerText = lengthInput.value));
+
+form.addEventListener('submit', event => {
+	event.preventDefault();
+	generatePassword(event.target, event.target.length.value);
+});
+
+copyButton.addEventListener('click', () => {
+	copyToClipboard('#password');
+});
